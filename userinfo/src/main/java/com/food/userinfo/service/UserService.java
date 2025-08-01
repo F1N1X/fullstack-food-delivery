8e3b1e9f9dd85c99322967f3cae748485b8dc726
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-01 22:32:34 +0200

    add service

---
+++ userinfo/src/main/java/com/food/userinfo/service/UserService.java
@@ -0,0 +1,11 @@
+package com.food.userinfo.service;
+
+import com.food.userinfo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepo userRepo;
+}
