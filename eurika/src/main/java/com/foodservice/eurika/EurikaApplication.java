a2c64d8fb0b97a1bae54aed58ac3687c229f1996
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-01 21:47:55 +0200

    init

---
+++ eurika/src/main/java/com/foodservice/eurika/EurikaApplication.java
@@ -0,0 +1,13 @@
+package com.foodservice.eurika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurikaApplication.class, args);
+	}
+
+}
