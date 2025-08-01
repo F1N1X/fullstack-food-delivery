feafffa5be62ffac2b02780ed6011db602b688d7
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-01 21:54:45 +0200

    config the server

--- src/main/java/com/foodservice/eurika/EurikaApplication.java
+++ src/main/java/com/foodservice/eurika/EurikaApplication.java
@@ -2,8 +2,10 @@ package com.foodservice.eurika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurikaApplication {
 
 	public static void main(String[] args) {
