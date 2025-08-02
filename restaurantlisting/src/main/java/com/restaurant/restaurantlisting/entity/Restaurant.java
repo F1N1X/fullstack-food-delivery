cc21cc255e79ab580fd2973d761b5b1d1f16cc1b
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-02 11:01:43 +0200

    add Repo
    use Interface for crud-operations and set ID + generate-strategy for objects

--- restaurantlisting/src/main/java/com/restaurant/restaurantlisting/entity/Restaurant.java
+++ restaurantlisting/src/main/java/com/restaurant/restaurantlisting/entity/Restaurant.java
@@ -1,6 +1,9 @@
 package com.restaurant.restaurantlisting.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String address;
---
+++ restaurantlisting/src/main/java/com/restaurant/restaurantlisting/repo/RestaurantRepo.java
@@ -0,0 +1,9 @@
+package com.restaurant.restaurantlisting.repo;
+
+import com.restaurant.restaurantlisting.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepo extends JpaRepository<Restaurant, Integer> {
+}
