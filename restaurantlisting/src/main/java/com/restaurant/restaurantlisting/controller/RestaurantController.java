97d1ad7501dfacb7dc2a431446d42ae0170465b7
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-02 12:05:39 +0200

    Controller fetchAll

---
+++ restaurantlisting/src/main/java/com/restaurant/restaurantlisting/controller/RestaurantController.java
@@ -0,0 +1,27 @@
+package com.restaurant.restaurantlisting.controller;
+
+import com.restaurant.restaurantlisting.dto.RestaurantDTO;
+import com.restaurant.restaurantlisting.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+    @GetMapping("/fetchAllRestaurants")
+    public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants() {
+        List<RestaurantDTO> allRestaurants = restaurantService.findAllRestaurants();
+        return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
+    }
+}
