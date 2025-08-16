e049a5c7bd9966209490be9b7b1923aa3df7aa8c
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-16 13:55:48 +0200

    Annotation Processing and fix errors by mapping

--- restaurantlisting/src/main/java/com/restaurant/restaurantlisting/mapping/RestaurantMapper.java
+++ restaurantlisting/src/main/java/com/restaurant/restaurantlisting/mapping/RestaurantMapper.java
@@ -1,12 +1,12 @@
 package com.restaurant.restaurantlisting.mapping;
 
-
 import com.restaurant.restaurantlisting.dto.RestaurantDTO;
 import com.restaurant.restaurantlisting.entity.Restaurant;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+
+@Mapper()
 public interface RestaurantMapper {
     RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
 
