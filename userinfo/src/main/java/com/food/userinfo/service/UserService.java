3b98068e69aef9353bad2a9347e34f8e681e64a4
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-17 19:23:35 +0200

    optimize

--- userinfo/src/main/java/com/food/userinfo/service/UserService.java
+++ userinfo/src/main/java/com/food/userinfo/service/UserService.java
@@ -23,9 +23,7 @@ public class UserService {
 
     public ResponseEntity<UserDTO> fetchUserDetailsById(Integer userId) {
         Optional<User> fetchedUser = userRepo.findById(userId);
-        if (fetchedUser.isPresent())
-            return new ResponseEntity<>(UserMapper.INSTANCE.mapUserToUserDTO(fetchedUser.get()), HttpStatus.OK);
-        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        return fetchedUser.map(user -> new ResponseEntity<>(UserMapper.INSTANCE.mapUserToUserDTO(user), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
 
     }
 }
