1813841ebd5ee84c57a5ee4ab9abd64d4667f058
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-23 14:48:43 +0200

    FoodCatalogController
    basis implementation for the controller and the path

---
+++ foodcatalogue/src/main/java/com/food/foodcatalogue/controller/FoodCatalogController.java
@@ -0,0 +1,10 @@
+package com.food.foodcatalogue.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/foodCatalogue")
+public class FoodCatalogController {
+}
