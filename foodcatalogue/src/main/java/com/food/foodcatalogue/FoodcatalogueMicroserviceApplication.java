8943581ad5e15f309d5be502d2bf0c868e0117c7
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-23 13:56:57 +0200

    Add new MS-FoodCatalogue
    new codebase add for MS to list food in a restaurant

---
+++ foodcatalogue/src/main/java/com/food/foodcatalogue/FoodcatalogueMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.food.foodcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcatalogueMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcatalogueMicroserviceApplication.class, args);
+	}
+
+}
