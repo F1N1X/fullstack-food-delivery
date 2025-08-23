0d30f577d3e9afcbb840d5680d7655cdad04e499
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-23 14:47:02 +0200

    FoodCatalogePage
    Data for hold the Items on a specific restaurant

---
+++ foodcatalogue/src/main/java/com/food/foodcatalogue/dto/FoodCataloguePage.java
@@ -0,0 +1,17 @@
+package com.food.foodcatalogue.dto;
+
+import com.food.foodcatalogue.entity.FoodItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodCataloguePage {
+
+    private List<FoodItem> foodItems;
+    private Restaurant restaurant;
+}
