c537eb50b7cb55603d3d7128e28163b1798db821
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-23 14:46:31 +0200

    MapperInterface the FoodItem
    from db to DTO and convert back

---
+++ foodcatalogue/src/main/java/com/food/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,19 @@
+package com.food.foodcatalogue.mapper;
+
+
+import com.food.foodcatalogue.dto.FoodItemDTO;
+import com.food.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+    FoodItemDTO mapFoodItemToFoodItemDTO(FoodItem foodItem);
+
+
+}
