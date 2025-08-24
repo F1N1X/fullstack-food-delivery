9021d99ece447764d92c9cb2fd7321f10d401f4e
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-24 19:05:35 +0200

    fix wrong api

--- foodcatalogue/src/main/java/com/food/foodcatalogue/controller/FoodCatalogController.java
+++ foodcatalogue/src/main/java/com/food/foodcatalogue/controller/FoodCatalogController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class FoodCatalogController {
     FoodCatalogueService foodCatalogueService;
 
     @PostMapping("/addFoodItem")
-    public ResponseEntity<FoodItemDTO> addFoodItem(@RequestMapping FoodItemDTO foodItemDTO) {
+    public ResponseEntity<FoodItemDTO> addFoodItem(@RequestBody FoodItemDTO foodItemDTO) {
         FoodItemDTO foodItemSaved = foodCatalogueService.addFoodItem(foodItemDTO);
         return new ResponseEntity<>(foodItemSaved, HttpStatus.OK);
     }
