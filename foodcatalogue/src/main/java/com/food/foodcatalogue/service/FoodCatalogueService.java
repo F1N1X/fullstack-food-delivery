951a937893a005c517a49f1724f540a32e9c95b1
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-08-24 18:49:35 +0200

    Insert the element in DB from Service

--- foodcatalogue/src/main/java/com/food/foodcatalogue/service/FoodCatalogueService.java
+++ foodcatalogue/src/main/java/com/food/foodcatalogue/service/FoodCatalogueService.java
@@ -1,7 +1,10 @@
 package com.food.foodcatalogue.service;
 
 
+import com.food.foodcatalogue.dto.FoodItemDTO;
 import com.food.foodcatalogue.entity.FoodItem;
+import com.food.foodcatalogue.mapper.FoodItemMapper;
+import com.food.foodcatalogue.repo.FoodItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,10 @@ import org.springframework.stereotype.Repository;
 public class FoodCatalogueService {
 
     @Autowired
-    FoodItem footItemRepo;
+    FoodItemRepo footItemRepo;
+
+    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+        FoodItem foodItemSaved = footItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
+        return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSaved);
+    }
 }
